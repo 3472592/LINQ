cd7aa5432e7ca11976fc72a0d2d589af3ce2720d
3472592 <3472592@gmail.com>
2024-06-27 05:52:33 -0400

    Add files via upload

---
+++ Versatile_LINQ/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Versatile_LINQ;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string[] sandwiches = { "ham and cheese", "salami with mayo",
+                                "turkey and swiss", "chicken cutlet" };
+
+        var sandwichesOnRye =
+            from sandwich in sandwiches
+            select sandwich + " on rye";
+
+        foreach (var sandwich in sandwichesOnRye)
+        {
+            Console.WriteLine(sandwich);
+            Console.ReadLine();
+        }
+    }
+}
